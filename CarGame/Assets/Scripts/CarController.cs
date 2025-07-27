a37d6d83eff99ad94fa61cef3706d56045cc7ecd
Vishwas Sharma <officiallyacoder@gmail.com>
2025-07-27 23:43:17 +0530

    car follower

--- CarGame/Assets/Scripts/CarController.cs
+++ CarGame/Assets/Scripts/CarController.cs
@@ -6,12 +6,22 @@ public class CarController : MonoBehaviour
 {
     
     public float rotationSpeed, moveSpeed;
+    public Transform Cartransform;
 
     void Update()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
         Debug.Log("horizontal:" + horizontalMovement + "vertical:" + verticalMovement);
+
+        if(Mathf.Abs(horizontalMovement) > 0.01f)
+        {
+            Cartransform.Rotate(0f,0f,-1*rotationSpeed*horizontalMovement);
+        }
+        if(Mathf.Abs(verticalMovement) > 0.01f)
+        {
+            Cartransform.Translate(0f,moveSpeed * verticalMovement,0f);
+        }
 
     }
 }
